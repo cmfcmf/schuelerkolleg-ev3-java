8536fd47dd8d32cf0ecc7f3a7112f64163216120
Christian Flach <cmfcmf.flach@gmail.com>
2016-12-13 13:17:14 +0100

    Fix encoding.

--- src/de/hpi/schuelerkolleg/ev3/Pilot.java
+++ src/de/hpi/schuelerkolleg/ev3/Pilot.java
@@ -10,7 +10,7 @@ import lejos.robotics.navigation.MovePilot;
 public class Pilot {
 
 	public static MovePilot initPilot() {
-		return initPilot(5.2); //f�r Standardwerte (Speed) 5.1 benutzen
+		return initPilot(5.2); //fuer Standardwerte (Speed) 5.1 benutzen
 	}
 	
 	public static MovePilot initPilot(double diameter) {
