897caeee96f20a1182d46449c9ff4ad9d98a6fe0
Christian Flach <cmfcmf.flach@gmail.com>
2016-12-13 13:14:29 +0100

    Fix "Cannot open port" error when using the MovePilot and another regulated motor.

--- src/de/hpi/schuelerkolleg/ev3/Pilot.java
+++ src/de/hpi/schuelerkolleg/ev3/Pilot.java
@@ -1,6 +1,7 @@
 package de.hpi.schuelerkolleg.ev3;
 
-import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
@@ -9,12 +10,14 @@ import lejos.robotics.navigation.MovePilot;
 public class Pilot {
 
 	public static MovePilot initPilot() {
-		return initPilot(5.2); //für Standardwerte (Speed) 5.1 benutzen
+		return initPilot(5.2); //fï¿½r Standardwerte (Speed) 5.1 benutzen
 	}
 	
 	public static MovePilot initPilot(double diameter) {
-		Wheel wheel1 = WheeledChassis.modelWheel(Motor.B, diameter).offset(-6);
-		Wheel wheel2 = WheeledChassis.modelWheel(Motor.C, diameter).offset(6);
+		EV3LargeRegulatedMotor b = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor c = new EV3LargeRegulatedMotor(MotorPort.C);
+		Wheel wheel1 = WheeledChassis.modelWheel(b, diameter).offset(-6);
+		Wheel wheel2 = WheeledChassis.modelWheel(c, diameter).offset(6);
 		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
 		MovePilot pilot = new MovePilot(chassis);
 		pilot.setLinearSpeed(20);
